ead747187c1f6cc409061bdc770fd06ad74b4ebb
Jubel-BRACU <mdjubel31@gmai.com>
2020-07-17 22:18:28 +0600

    new path add

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse 
 
+def hi(request):
+    return HttpResponse('hello, how are you ')
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+   
+    path('test', hi)
 ]
